1b5bd39e29b0a0e1e0ec0674ad4274346b42780a
2021dmdrl <rotorldi1541@gmail.com>
2021-02-14 16:59:34 +0900

    아답터들은안됨

---
+++ app/src/main/java/com/gmail/rotorldi1541/letsgocamping/IdFindData.java
@@ -0,0 +1,32 @@
+package com.gmail.rotorldi1541.letsgocamping;
+
+public class IdFindData {
+    private int num;
+    private String id;
+    private String date;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+}
