463622233e8e703f79e1a09aacdfcf00348a5726
dmdrl <rotorldi1541@gmail.com>
2021-02-15 11:26:41 +0900

    text2

---
+++ app/src/main/java/com/gmail/rotorldi1541/letsgocamping/IogActivity.java
@@ -0,0 +1,30 @@
+package com.gmail.rotorldi1541.letsgocamping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class IogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iog);
+    }
+
+    public void idcktrl(View view) {
+        //아이디 비밀번호 찾기
+        Intent intent=new Intent(this,FindIdPasswordActivity.class);
+        startActivity(intent);
+    }
+
+    public void djqfhem(View view) {
+        //회원가입
+    }
+
+    public void main(View view) {
+        //메인가기
+    }
+}
