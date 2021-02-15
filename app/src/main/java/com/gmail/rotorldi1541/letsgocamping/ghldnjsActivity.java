cdd6908d0c497a77d082bf06fde70ef0582cf6fc
dmdrl <rotorldi1541@gmail.com>
2021-02-15 11:47:29 +0900

    text3

--- app/src/main/java/com/gmail/rotorldi1541/letsgocamping/ghldnjsActivity.java
+++ app/src/main/java/com/gmail/rotorldi1541/letsgocamping/ghldnjsActivity.java
@@ -2,7 +2,9 @@ package com.gmail.rotorldi1541.letsgocamping;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ghldnjsActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ghldnjsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ghldnjs);
+    }
+
+    public void mane2(View view) {
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
