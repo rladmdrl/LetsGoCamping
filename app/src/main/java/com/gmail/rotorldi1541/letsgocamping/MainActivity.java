8ab1a0dcd43aab6000e49b583752a197a2bb9d79
dmdrl <rotorldi1541@gmail.com>
2021-02-15 13:30:36 +0900

    vkdlsjf

--- app/src/main/java/com/gmail/rotorldi1541/letsgocamping/MainActivity.java
+++ app/src/main/java/com/gmail/rotorldi1541/letsgocamping/MainActivity.java
@@ -3,6 +3,7 @@ package com.gmail.rotorldi1541.letsgocamping;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Camping(View view) {
+        //캠핑장
+    }
+
+    public void restaurant(View view) {
+        //음식점
+    }
+
+    public void SuppliesStore(View view) {
+        //캠핑용품점
+    }
+
+    public void parkingLot(View view) {
+        //주차장
     }
 }
