e20b39c02946caf8e37bd19c92ff34a4aff5ef56
dmdrl <rotorldi1541@gmail.com>
2021-02-18 16:47:26 +0900

    EditText

---
+++ app/src/main/java/com/gmail/rotorldi1541/letsgocamping/PostActivityt.java
@@ -0,0 +1,14 @@
+package com.gmail.rotorldi1541.letsgocamping;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostActivityt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_activityt);
+    }
+}
