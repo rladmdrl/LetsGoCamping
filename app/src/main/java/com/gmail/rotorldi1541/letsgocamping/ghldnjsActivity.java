d097b1ec4f88cfa7a25abb23a9a4cfde4b36ef8a
dmdrl <rotorldi1541@gmail.com>
2021-02-24 14:52:19 +0900

    어플 아이콘

--- app/src/main/java/com/gmail/rotorldi1541/letsgocamping/ghldnjsActivity.java
+++ app/src/main/java/com/gmail/rotorldi1541/letsgocamping/ghldnjsActivity.java
@@ -5,16 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class ghldnjsActivity extends AppCompatActivity {
+    EditText etName,etNum,etEme,etId,etPwd,etPwd2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ghldnjs);
+        etName=findViewById(R.id.et_name);
+        etNum=findViewById(R.id.et_num);
+        etEme=findViewById(R.id.et_eme);
+        etId=findViewById(R.id.et_id);
+        etPwd=findViewById(R.id.et_pass1);
+        etPwd2=findViewById(R.id.et_pass2);
     }
 
     public void mane2(View view) {
+
+
         Intent intent=new Intent(this,IogActivity.class);
         startActivity(intent);
     }
